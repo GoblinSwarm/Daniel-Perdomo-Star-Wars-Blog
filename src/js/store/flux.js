2a0f6abe3437409fb5704339dd72b97122e89d46
Daniel Perdomo <136826084+GoblinSwarm@users.noreply.github.com>
2024-07-12 04:35:05 +0000

    Fecthing all data

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -12,21 +12,21 @@ const getState = ({ getStore, getActions, setStore }) => {
 					let response = await fetch(`${dataUrl}`)
 					let data = await response.json()
 
-				if(response.ok){
-					return data; 
-				} else {
-					console.log(`Something bad happened at loolapalloza and ${data}`)
-				} 
-				} catch(error) {
+					if (response.ok) {
+						return data;
+					} else {
+						console.log(`Something bad happened at loolapalloza and ${data}`)
+					}
+				} catch (error) {
 					console.log(error);
 				}
 			},
+
 			getAllPeople: async () => {
 				try {
 					let response = await fetch(`${getStore().urlBase}/people`)
 					let data = await response.json()
-					let peopleInPage = []
-						// Esta guardando bien con esto ya
+
 					if (response.ok) {
 						let peopleInPage = await Promise.all(
 							data.results.map(async (item) => {
@@ -39,7 +39,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 						console.log(`Respuesta de la funcion getAllPeople: ${data}, ${response}`)
 					}
 
-				} catch(error) {
+				} catch (error) {
 					console.log("Error trying to bring back the info: ", error)
 				}
 			},
@@ -49,28 +49,41 @@ const getState = ({ getStore, getActions, setStore }) => {
 					let response = await fetch(`${getStore().urlBase}/vehicles`)
 					let data = await response.json()
 
-					if(response.ok){
-						setStore({vehicles: data.vehicles})
+					if (response.ok) {
+						let vehicleInPage = await Promise.all(
+							data.results.map(async (item) => {
+								let dataUrl = item.url;
+								return await getActions().getObjectbyID(dataUrl);
+							})
+						);
+						setStore({ vehicles: vehicleInPage });
 					} else {
 						console.log(`Respuesta de la funcion getAllVehicles: ${data}, ${response}`)
 					}
 
-				} catch(error) {
+				} catch (error) {
 					console.log("Error trying to bring back the info: ", error)
 				}
 			},
+
 			getAllPlanets: async () => {
 				try {
 					let response = await fetch(`${getStore().urlBase}/planets`)
 					let data = await response.json()
 
-					if(response.ok){
-						setStore({planets: data.planets})
+					if (response.ok) {
+						let planetInPage = await Promise.all(
+							data.results.map(async (item) => {
+								let dataUrl = item.url;
+								return await getActions().getObjectbyID(dataUrl);
+							})
+						);
+						setStore({ planets: planetInPage });
 					} else {
 						console.log(`Respuesta de la funcion getAllPlanets: ${data}, ${response}`)
 					}
 
-				} catch(error) {
+				} catch (error) {
 					console.log("Error trying to bring back the info: ", error)
 				}
 			}
