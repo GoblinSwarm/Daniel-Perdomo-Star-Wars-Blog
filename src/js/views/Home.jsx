417fb510cacf5633e04390d792da98415cb2187a
Daniel Perdomo <136826084+GoblinSwarm@users.noreply.github.com>
2024-07-12 04:29:13 +0000

    Added front for 3 categories

--- src/js/views/Home.jsx
+++ src/js/views/Home.jsx
@@ -1,80 +1,91 @@
 import React, { useContext } from "react";
-import { NavLink } from "react-router-dom";
 import { Context } from "../store/appContext";
-
 import "../../styles/home.css";
 
-
 export const Home = () => {
-	const { store, actions } = useContext(Context);
-    const { people } = store;
-	const { vehicles } = store;
-	const { planets } = store;
+	const { store } = useContext(Context);
+	const { people, vehicles, planets } = store;
 
 	return (
 		<div className="container text-start">
 			<div className="row">
 				<div className="col-10 align-self-center">
 					<h2>Characters</h2>
-					{people.map((person, index) => (
-					<div className="text-center mt-5" key={index}>
-						<div className="card" style={{width: "18rem"}}>
-	  						<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTOU1Z2x-YVbmqhk1jiiiy55zDEpi91Y0eYg&s" className="card-img-top" />
-  							<div className="card-body">
-    							<h5 className="card-title">Name: {person.name}</h5>
-    							<p className="card-text">Gender: {person.gender}</p> 
-								<p className="card-text">Hair-color: {person.hair_color}</p>
-								<p className="card-text">Eye-color: {person.eye_color}</p> 
-								<div className="buttons">
-	    							<a href="#" className="btn btn-primary">Learn More!</a>
-									<a href="#" className="btn btn-primary"><i className="far fa-heart"></i></a>
-								</div>
-  							</div>
-						</div>
-					</div>
-					))}
-					<div>
-						<br />
-						<h2>Planets</h2>
-						<div className="text-center mt-5">
-							<div className="card" style={{width: "18rem"}}>
-  								<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTOU1Z2x-YVbmqhk1jiiiy55zDEpi91Y0eYg&s" className="card-img-top" />
-  								<div className="card-body">
-    								<h5 className="card-title">Name:</h5>
-    								<p className="card-text">Gender:</p> 
-									<p className="card-text">Hair-color:</p>
-									<p className="card-text">Eye-color:</p> 
-									<div className="buttons">
-			    						<a href="#" className="btn btn-primary">Learn More!</a>
-										<a href="#" className="btn btn-primary"><i className="far fa-heart"></i></a>
+					<div className="overflow-auto">
+						<div className="d-flex flex-nowrap">
+							{people.map((person, index) => (
+								<div className="text-center mt-5" key={index}>
+									<div className="card" style={{ width: "18rem" }}>
+										<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTOU1Z2x-YVbmqhk1jiiiy55zDEpi91Y0eYg&s" className="card-img-top" alt="Character" />
+										<div className="card-body">
+											<h5 className="card-title">Name: {person.result.properties.name}</h5>
+											<p className="card-text">Gender: {person.result.properties.gender}</p>
+											<p className="card-text">Hair-color: {person.result.properties.hair_color}</p>
+											<p className="card-text">Eye-color: {person.result.properties.eye_color}</p>
+											<div className="buttons">
+												<a href="#" className="btn btn-primary">Learn More!</a>
+												<a href="#" className="btn btn-primary"><i className="far fa-heart"></i></a>
+											</div>
+										</div>
 									</div>
-  								</div>
-							</div>
+								</div>
+							))}
 						</div>
 					</div>
-				<div>
-					<br />
-					<h2>Vehicles</h2>
-					<div className="text-center mt-5">
-						<div className="card" style={{width: "18rem"}}>
-  							<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTOU1Z2x-YVbmqhk1jiiiy55zDEpi91Y0eYg&s" className="card-img-top" />
-  							<div className="card-body">
-    							<h5 className="card-title">Name:</h5>
-    							<p className="card-text">Gender:</p> 
-								<p className="card-text">Hair-color:</p>
-								<p className="card-text">Eye-color:</p> 
-								<div className="buttons">
-	    							<a href="#" className="btn btn-primary">Learn More!</a>
-									<a href="#" className="btn btn-primary"><i className="far fa-heart"></i></a>
+				</div>
+
+				<div className="col-10 align-self-center">
+					<h2>Planets</h2>
+					<div className="overflow-auto">
+						<div className="d-flex flex-nowrap">
+							{planets.map((planet, index) => (
+								<div className="text-center mt-5" key={index}>
+									<div className="card" style={{ width: "18rem" }}>
+										<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTOU1Z2x-YVbmqhk1jiiiy55zDEpi91Y0eYg&s" className="card-img-top" alt="Planet" />
+										<div className="card-body">
+											<h5 className="card-title">Name: {planet.result.properties.name}</h5>
+											<p className="card-text">Terrain: {planet.result.properties.terrain}</p>
+											<p className="card-text">Climate: {planet.result.properties.climate}</p>
+											<p className="card-text">Gravity: {planet.result.properties.gravity}</p>
+											<div className="buttons">
+												<a href="#" className="btn btn-primary">Learn More!</a>
+												<a href="#" className="btn btn-primary"><i className="far fa-heart"></i></a>
+											</div>
+										</div>
+									</div>
 								</div>
-  							</div>
+							))}
+						</div>
+					</div>
+				</div>
+
+				<div className="col-10 align-self-center">
+					<h2>Vehicles</h2>
+					<div className="overflow-auto">
+						<div className="d-flex flex-nowrap">
+							{vehicles.map((vehicle, index) => (
+								<div className="text-center mt-5" key={index}>
+									<div className="card" style={{ width: "18rem" }}>
+										<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTOU1Z2x-YVbmqhk1jiiiy55zDEpi91Y0eYg&s" className="card-img-top" alt="Vehicle" />
+										<div className="card-body">
+											<h5 className="card-title">Name: {vehicle.result.properties.name}</h5>
+											<p className="card-text">Model: {vehicle.result.properties.model}</p>
+											<p className="card-text">Class: {vehicle.result.properties.vehicle_class}</p>
+											<p className="card-text">Crew: {vehicle.result.properties.crew}</p>
+											<div className="buttons">
+												<a href="#" className="btn btn-primary">Learn More!</a>
+												<a href="#" className="btn btn-primary"><i className="far fa-heart"></i></a>
+											</div>
+										</div>
+									</div>
+								</div>
+							))}
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	)
+	);
 };
 
 export default Home;
